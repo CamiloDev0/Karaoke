2cf547eb2cabb89303ec98d0a1e4a9a05c9de8a9
Andres Gutierrez <gagutierrez454@soy.sena.edu.co>
2023-11-27 18:59:53 -0500

    Fix to BUILD command on Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { KarolGApp } from "./KarolGApp";
 import { store } from "./store";
-import WebcamVideo from "./webcam/WebcamVideo";
+// import WebcamVideo from "./webcam/WebcamVideo";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

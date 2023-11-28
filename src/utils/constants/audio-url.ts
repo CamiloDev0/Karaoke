f42886eec661ef8bf916241ced413ab128b4dcde
CamiloDev0 <130998699+CamiloDev0@users.noreply.github.com>
2023-11-27 20:24:06 -0500

    Audios

--- src/utils/constants/audio-url.ts
+++ src/utils/constants/audio-url.ts
@@ -1,6 +1,6 @@
 import { TAudioUrl } from "../../store";
 
 export const AudioUrl: TAudioUrl = {
-  bonito: "./assets/audios/provenza.mp3",
-  provenza: "./assets/audios/provenza.mp3",
+  bonito: "./assets/audios/ex.mp3",
+  provenza: "./assets/audios/cora.mp3",
 };
